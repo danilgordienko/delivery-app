a955075629d1147a02248a45c27d60d0fc1eafac
Danil Gordienko <danilgordienko212@gmail.com>
2026-02-04 22:00:37 +0300

    add vlidation to accepting delivery

--- src/main/java/ru/danilgordienko/delivery_app/model/dto/DeliveryRequestDto.java
+++ src/main/java/ru/danilgordienko/delivery_app/model/dto/DeliveryRequestDto.java
@@ -1,5 +1,7 @@
 package ru.danilgordienko.delivery_app.model.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,6 +10,8 @@ import java.util.List;
 @Data
 public class DeliveryRequestDto {
     private Long providerId;
+    @NotNull
     private LocalDate deliveryDate;
+    @Size(min = 1, message = "count of products must be more than 0")
     private List<DeliveryProductDto> products;
 }
